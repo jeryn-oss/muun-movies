7bb959576fc2a7b63bb40f160e1e8c6594786537
jeryn <81574573+jeryn-oss@users.noreply.github.com>
2023-10-07 18:47:59 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,9 +1,9 @@
-const queryString = window.location.search;
+r=c do const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 if (!urlParams.has('id')) {
     window.location.href = "index.html"
 }
-$(".player").append('<iframe src="https://vidsrc.to/embed/movie/' + urlParams.get('id') + '" allow="autoplay; fullscreen" allowfullscreen="yes" frameborder="no" scrolling="no" ><iframe>')
+$(".player").append('<iframe src="https://vidsrc.to/embed/movie/' + urlParams.get('id') + '" ><iframe>')
 
 $(".back-btn").on("click", function () {
     window.history.back();
